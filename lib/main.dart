12033543986e3c08b7a96b2fe3733ff9ad9e47ea
Athirah <nrathrh57@gmail.com>
2020-03-31 03:43:54 +0800

    add notification 2

--- lib/main.dart
+++ lib/main.dart
@@ -16,150 +16,147 @@ class MyApp extends StatelessWidget {
 }
 
 class Menu extends StatelessWidget {
-void _doNothing(){}
+  void _doNothing() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: Drawer(
-          child: ListView(
-            children: <Widget>[
-              DrawerHeader(
-                decoration: BoxDecoration(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage('assets/bg.png'),
                   fit: BoxFit.cover,
-              ),
+                ),
               ),
               child: Image(
-                 alignment: Alignment.center,
-                  fit:  BoxFit.contain,
-                  image: AssetImage('assets/main.gif'),
+                alignment: Alignment.center,
+                fit: BoxFit.contain,
+                image: AssetImage('assets/main.gif'),
               ),
-                ),
-                  ListTitle(Icons.person,'Profile', _doNothing ),
-                  ListTitle(Icons.notifications,'Notification', _doNothing ),
-                  ListTitle(Icons.settings,'Setting', _doNothing ),
-                  ListTitle(Icons.exit_to_app,'Logout', Notification ),
-            ],
-          ),
-        ),  
-      appBar: AppBar(   
-        leading: IconButton(icon: Icon(Icons.settings), onPressed: _doNothing),   
-        title: Text('Welcome Corona!'),      
-        backgroundColor: Colors.red[300],
-        actions: <Widget> [          
-          // Material(            
-          //   child: InkWell(
-          //     onTap: _doNothing,              
-          //     child:  Image(
-          //         alignment: Alignment.topRight,
-          //         fit:  BoxFit.contain,
-          //         image: AssetImage('assets/logout.png'),
-                  
-          // ),
-          //   )
-          // ),  
-        ]
-        ), 
-        body: Container(
+            ),
+            ListTitle(Icons.person, 'Profile', _doNothing),
+            ListTitle(Icons.notifications, 'Notification', _doNothing),
+            ListTitle(Icons.settings, 'Setting', _doNothing),
+            ListTitle(Icons.exit_to_app, 'Logout', _doNothing),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+          leading:
+              IconButton(icon: Icon(Icons.settings), onPressed: _doNothing),
+          title: Text('Welcome Corona!'),
+          backgroundColor: Colors.red[300],
+          actions: <Widget>[
+            // Material(
+            //   child: InkWell(
+            //     onTap: _doNothing,
+            //     child:  Image(
+            //         alignment: Alignment.topRight,
+            //         fit:  BoxFit.contain,
+            //         image: AssetImage('assets/logout.png'),
+
+            // ),
+            //   )
+            // ),
+          ]),
+      body: Container(
         padding: const EdgeInsets.all(10.0),
         height: double.infinity,
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/bg.png'),
             fit: BoxFit.cover,
+          ),
         ),
-        ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("Score: 0", style: TextStyle(fontSize: 20, color: Colors.white)),
-              Image.asset(
-                'assets/main.gif',
-                height: 190,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Score: 0",
+                style: TextStyle(fontSize: 20, color: Colors.white)),
+            Image.asset(
+              'assets/main.gif',
+              height: 190,
+            ),
+            // Container(
+            //   height: 170.0,
+            //   decoration: BoxDecoration(
+            //       image: DecorationImage(
+            //           image: AssetImage('assets/main.gif'))
+            //  ),
+            // ),
+            // Image(image: AssetImage("assets/ajax-main.gif")),
+            SizedBox(height: 20.0),
+            SizedBox(
+              height: 70,
+              width: double.infinity,
+              child: RaisedButton(
+                color: Colors.blue[300],
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(20.0),
+                child: Text("Body Part", style: TextStyle(fontSize: 25)),
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0)),
               ),
-          // Container(
-          //   height: 170.0,
-          //   decoration: BoxDecoration(
-          //       image: DecorationImage(
-          //           image: AssetImage('assets/main.gif'))
-          //  ),
-          // ),
-          // Image(image: AssetImage("assets/ajax-main.gif")),
-      SizedBox(height: 20.0),
-                SizedBox(
-                  height: 70,
-                  width: double.infinity,
-                  child: RaisedButton(
-                    color: Colors.blue[300],
-                    textColor: Colors.white,
-                    padding: const EdgeInsets.all(20.0),
-                    child: Text("Body Part", style: TextStyle(fontSize: 25)),
-                    onPressed: (){},
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0)
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10.0),
-                SizedBox(
-                  height: 70,
-                  width: double.infinity,
-                  child: RaisedButton(
-                    color: Colors.purple[200],
-                    textColor: Colors.white,
-                    padding: const EdgeInsets.all(20.0),
-                    child: Text("Animal", style: TextStyle(fontSize: 25)),
-                    onPressed: (){},
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0)
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10.0),
-                SizedBox(
-                  height: 70,
-                  width: double.infinity,
-                  child: RaisedButton(
-                    color: Colors.lightGreen[200],
-                    textColor: Colors.white,
-                    padding: const EdgeInsets.all(20.0),
-                    child: Text("Color", style: TextStyle(fontSize: 25)),
-                    onPressed: (){},
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0)
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10.0),
-                SizedBox(
-                  height: 70,
-                  width: double.infinity,
-                  child: RaisedButton(
-                    color: Colors.redAccent[100],
-                    textColor: Colors.white,
-                    padding: const EdgeInsets.all(20.0),
-                    child: Text("Sport", style: TextStyle(fontSize: 25)),
-                    onPressed: (){},
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0)
-                    ),
-                  ),
-                ),
-            ],),
+            ),
+            SizedBox(height: 10.0),
+            SizedBox(
+              height: 70,
+              width: double.infinity,
+              child: RaisedButton(
+                color: Colors.purple[200],
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(20.0),
+                child: Text("Animal", style: TextStyle(fontSize: 25)),
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0)),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            SizedBox(
+              height: 70,
+              width: double.infinity,
+              child: RaisedButton(
+                color: Colors.lightGreen[200],
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(20.0),
+                child: Text("Color", style: TextStyle(fontSize: 25)),
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0)),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            SizedBox(
+              height: 70,
+              width: double.infinity,
+              child: RaisedButton(
+                color: Colors.redAccent[100],
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(20.0),
+                child: Text("Sport", style: TextStyle(fontSize: 25)),
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0)),
+              ),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
 
 class ListTitle extends StatelessWidget {
-
   IconData icon;
   String text;
   Function onTap;
 
-  ListTitle(this.icon,this.text,this.onTap);
+  ListTitle(this.icon, this.text, this.onTap);
 
   @override
   Widget build(BuildContext context) {
@@ -171,7 +168,12 @@ class ListTitle extends StatelessWidget {
         ),
         child: InkWell(
           splashColor: Colors.orangeAccent,
-          onTap: onTap,
+          onTap: () {
+            return Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Notification()),
+            );
+          },
           child: Container(
             height: 70,
             child: Row(
@@ -181,13 +183,13 @@ class ListTitle extends StatelessWidget {
                   children: <Widget>[
                     Icon(icon),
                     Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        text, 
-                        style: TextStyle(
-                          fontSize: 20.0,
-                      ),)
-                      )
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          text,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                          ),
+                        ))
                   ],
                 ),
               ],
@@ -199,17 +201,49 @@ class ListTitle extends StatelessWidget {
   }
 }
 
-class Notification extends StatelessWidget{
+class Notification extends StatelessWidget {
+  
+  final items = List<String>.generate(5, (i) => "Message ${i + 1}");
 
-  Widget build(BuildContext context){
+  Function setState;
+
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Notifications"),
+        title: Text("Messages"),
+        backgroundColor: Colors.red[300],
       ),
-      body: Container(
-
-      ),
-    )
+      body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            Icon(Icons.delete);
+            return Dismissible(
+              key: Key(item),
+              onDismissed: (direction) {
+                setState(() {
+                  items.removeAt(index);
+                });
+                Scaffold.of(context)
+                    .showSnackBar(SnackBar(content: Text("$item dismissed")));
+              },
+              background: Container(color: Colors.red,),
+              child: Card(
+                color: const Color(0xFFFCE4EC),
+                child: ListTile(
+                leading: Icon(Icons.notifications_active),
+                title: Text('$item'),
+                subtitle: Text(
+                  'Collect more score to beat others. '
+                  '20/3/2020'
+                ),
+                isThreeLine: true,
+               // trailing: Icon(Icons.swap_horizontal_circle),
+              ),
+              ),
+            );
+          }, //itemBuilder
+        ),
+      );
   }
 }
-
