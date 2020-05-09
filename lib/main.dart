faba3e8c18d67137346c9ddb17d1fc78abb5b5f1
hasnatulaida <62786569+hasnatulaida@users.noreply.github.com>
2020-05-09 10:34:54 +0800

    string haha

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:splash_screen_demo/splash_screen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  String hahahahahah;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
